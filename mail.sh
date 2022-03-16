dd762399d48cd82a3948b5537bc85bfb603bee89
Ubuntu <ubuntu@ip-172-31-89-7.ec2.internal>
2022-03-16 09:51:30 +0000

    mail response

---
+++ mail.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+size=`df -h . | awk -F " " '(NR>1) {print $(NF-1)}' | sed 's/%/ /g'`
+if [ $size -gt 20 ]
+then
+	echo "memory reaches the trushold value" | mail -s "memory reached" ambareeshns@gmail.com
+fi
